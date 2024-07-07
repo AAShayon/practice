86dbba1ae4d3664a98017768d00070d2364512ea
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-07-08 01:28:06 +0600

    next

--- lib/modules/view/products_screen.dart
+++ lib/modules/view/products_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:e_commerce_dummy_json/modules/viewModel/product_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class ProductsScreen extends StatefulWidget {
@@ -41,115 +42,50 @@ class _ProductsScreenState extends State<ProductsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('E-commerce Dummy', style: Theme.of(context).textTheme.labelMedium),
-      ),
-      body: Consumer<ProductViewModel>(
-        builder: (context, productViewModel, Widget? child) {
-          final products = productViewModel.products;
-          return Container(
-            color: Colors.white,
-            child: products.isNotEmpty
-                ? ListView.builder(
-              controller: scrollController,
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                final product = products[index];
-                return ListTile(
-                  leading: Text('${index + 1}'),
-                  title: Text('${product.title}'),
-                );
-              },
-            )
-                : const Center(child: CircularProgressIndicator()),
-          );
-        },
+    return RefreshIndicator(
+      onRefresh: ()async{
+        final product=Provider.of<ProductViewModel>(context,listen: false);
+        setState(() {
+ product.clearList();
+ product.resetLimit();
+        });
+        await  _loadData(context, isRefresh: true, isLoadMore: false);
+        Future<void>.delayed(const Duration(seconds: 2));
+
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('E-commerce Dummy', style: Theme.of(context).textTheme.labelMedium),
+        ),
+        body: Consumer<ProductViewModel>(
+          builder: (context, productViewModel, Widget? child) {
+            final products = productViewModel.products;
+            return ListView(
+                controller: scrollController,
+              shrinkWrap: true,
+              physics: AlwaysScrollableScrollPhysics(),
+              children:[ products.isNotEmpty
+                  ? ListView.builder(
+                physics:  NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return ListTile(
+                    leading: Text('${index + 1}'),
+                    title: Text('${product.title}'),
+                  );
+                },
+              )
+                  : const Center(child: CircularProgressIndicator()),
+              SizedBox(height: 30.h),
+                productViewModel.isLoading ? const Center(child: CircularProgressIndicator(),):SizedBox.shrink(),
+              ]
+            );
+          },
+        ),
       ),
     );
   }
 }
 
-// import 'package:e_commerce_dummy_json/modules/viewModel/product_view_model.dart';
-// import 'package:e_commerce_dummy_json/view/widgets/no_internet_connection.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:internet_connection_checker/internet_connection_checker.dart';
-// import 'package:provider/provider.dart';
-// class ProductsScreen extends StatefulWidget {
-//   const ProductsScreen({super.key});
-//
-//   @override
-//   State<ProductsScreen> createState() => _ProductsScreenState();
-// }
-//
-// class _ProductsScreenState extends State<ProductsScreen> {
-//   final ScrollController scrollController=ScrollController();
-//   @override
-//   void initState() {
-//     super.initState();
-//     final productViewModel=Provider.of<ProductViewModel>(context,listen: false);
-//     productViewModel.resetLimit();
-//     productViewModel.clearList();
-//     _loadData(context,isLoadMore: false,isRefresh: true);
-//     scrollController.addListener(_scrollController);
-//   }
-//   Future<void> _loadData(BuildContext context,{required bool isLoadMore,required bool isRefresh})async{
-//     final productViewModel=Provider.of<ProductViewModel>(context,listen: false);
-//     await productViewModel.getAllProducts(context, isLoadMore: isLoadMore);
-//   }
-//   void _scrollController(){
-//     final productViewModel=Provider.of<ProductViewModel>(context,listen: false);
-//     if(!productViewModel.isLoading && scrollController.position.pixels == scrollController.position.maxScrollExtent){
-//       productViewModel.incrementPage();
-//       _loadData(context,isLoadMore: true, isRefresh: false);
-//     }
-//     print('Scrolling');
-//
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Consumer<ProductViewModel>(builder: (context,productViewModel,child){
-//       return  Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Ecommerce DummyJson'),
-//         ),
-//         body: Padding(
-//           padding:  EdgeInsets.symmetric(vertical: 10.h,horizontal: 10.w),
-//           child: Container(
-//             height: 800.h,
-//             width: 380.w,
-//             decoration: BoxDecoration(
-//               borderRadius: BorderRadius.circular(10),
-//               border: Border.all(
-//                 color: Colors.black,width: 1.w
-//               ),
-//               color: Colors.transparent,
-//             ),
-//             child: ListView(
-//               controller: scrollController,
-//               children: [
-//                 SizedBox(height: 20.h,),
-//                 productViewModel.products.isNotEmpty ?
-//                 ListView.builder(
-//                   itemCount: productViewModel.products.length,
-//                     itemBuilder:(context ,index){
-//                     final product=productViewModel.products[index];
-//                       return ListTile(
-//                         leading: Text('${index +1 }',style: TextStyle(color: Colors.black),),
-//                         title: Text("${product.title}"),
-//                       );
-//                     } ):const Center(child: CircularProgressIndicator(),),
-//
-//               productViewModel.isLoading ? const Center(child: CircularProgressIndicator(),):const SizedBox.shrink(),
-//                 SizedBox(height: 30.h,)
-//               ],
-//             ),
-//           ),
-//         )
-//       );
-//
-//     });
-//   }
-// }
